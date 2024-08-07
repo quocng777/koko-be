0ff984dd4f4c7d449b67ab2a8f6cfea40c03f6e7
Quoc Nguyen <quocng777@gmail.com>
2024-08-07 19:22:41 +0700

    add cors config

---
+++ src/main/java/com/quocnguyen/koko/config/GlobalCorsConfig.java
@@ -0,0 +1,26 @@
+package com.quocnguyen.koko.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Quoc Nguyen on {8/7/2024}
+ */
+
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
